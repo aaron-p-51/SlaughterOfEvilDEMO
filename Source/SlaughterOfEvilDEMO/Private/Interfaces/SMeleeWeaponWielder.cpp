291400121c066ce5d891d781cf64d7da7bfb4cf1
aaron-p-51 <petrekaj@gmail.com>
2021-07-03 17:08:18 -0700

    Created SMeleeWeaponWielder class

---
+++ Source/SlaughterOfEvilDEMO/Private/Interfaces/SMeleeWeaponWielder.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/SMeleeWeaponWielder.h"
+
+// Add default functionality here for any ISMeleeWeaponWielder functions that are not pure virtual.
